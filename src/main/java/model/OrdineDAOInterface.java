e376d648104da712d4b2db569715569b52489ffc
Lstassano <lstassano44@gmail.com>
2024-07-01 18:14:48 +0200

    Aggiunta OrdineDAO e OrdineDAOInterface.

---
+++ src/main/java/model/OrdineDAOInterface.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.sql.SQLException;
+import java.util.List;
+import model.Ordine;
+
+public interface OrdineDAOInterface {
+    void insertOrdine(Ordine ordine) throws SQLException;
+    Ordine selectOrdine(String orderID);
+    List<Ordine> selectAllOrdini();
+    boolean deleteOrdine(String orderID) throws SQLException;
+    boolean updateOrdine(Ordine ordine) throws SQLException;
+}
