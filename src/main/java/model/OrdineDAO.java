4ef8ae236e7c7756f30c834f4aa4f656642838e2
Lstassano <lstassano44@gmail.com>
2024-07-01 22:58:24 +0200

    fix interface OrdineDAO.

--- src/main/java/model/OrdineDAO.java
+++ src/main/java/model/OrdineDAO.java
@@ -1,50 +1,53 @@
 package model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import model.Ordine;
 
-public class OrdineDAO implements OrdineDAOInterface{
-
-    private String jdbcURL = "jdbc:mysql://localhost:3306/tuo_database";
-    private String jdbcUsername = "tuo_username";
-    private String jdbcPassword = "tua_password";
-
-    private static final String INSERT_ORDINE_SQL = "INSERT INTO Ordini (orderID, dataOrdine, clientID, totale, stato) VALUES (?, ?, ?, ?, ?);";
-    private static final String SELECT_ORDINE_BY_ID = "SELECT orderID, dataOrdine, clientID, totale, stato FROM Ordini WHERE orderID = ?;";
-    private static final String SELECT_ALL_ORDINI = "SELECT * FROM Ordini;";
-    private static final String DELETE_ORDINE_SQL = "DELETE FROM Ordini WHERE orderID = ?;";
-    private static final String UPDATE_ORDINE_SQL = "UPDATE Ordini SET dataOrdine = ?, clientID = ?, totale = ?, stato = ? WHERE orderID = ?;";
+public class OrdineDAO implements OrdineDAOInterface {
+    private static final String INSERT_ORDINE_SQL = "INSERT INTO Ordini (orderID, dataOrdine, nomeCliente, cognomeCliente, cap, indirizzoDiConsegna, totale, stato) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String SELECT_ORDINE_BY_ID = "SELECT orderID, dataOrdine, nomeCliente, cognomeCliente, cap, indirizzoDiConsegna, totale, stato FROM Ordini WHERE orderID = ?";
+    private static final String SELECT_ALL_ORDINI = "SELECT * FROM Ordini";
+    private static final String DELETE_ORDINE_SQL = "DELETE FROM Ordini WHERE orderID = ?";
+    private static final String UPDATE_ORDINE_SQL = "UPDATE Ordini SET dataOrdine = ?, nomeCliente = ?, cognomeCliente = ?, cap = ?, indirizzoDiConsegna = ?, totale = ?, stato = ? WHERE orderID = ?";
 
     protected Connection getConnection() {
+        // Assicurati di sostituire queste variabili con le tue impostazioni del database
+        String jdbcURL = "jdbc:mysql://localhost:3306/tuo_database";
+        String jdbcUsername = "username";
+        String jdbcPassword = "password";
+
         Connection connection = null;
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
         } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         return connection;
     }
 
+    @Override
     public void insertOrdine(Ordine ordine) throws SQLException {
         try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(INSERT_ORDINE_SQL)) {
             preparedStatement.setString(1, ordine.getOrderID());
-            preparedStatement.setDate(2, java.sql.Date.valueOf(ordine.getDataOrdine()));
-            preparedStatement.setString(3, ordine.getClientID());
-            preparedStatement.setDouble(4, ordine.getTotale());
-            preparedStatement.setBoolean(5, ordine.isStato());
+            preparedStatement.setString(2, ordine.getDataOrdine());
+            preparedStatement.setString(3, ordine.getNomeCliente());
+            preparedStatement.setString(4, ordine.getCognomeCliente());
+            preparedStatement.setString(5, ordine.getCap());
+            preparedStatement.setString(6, ordine.getIndirizzoDiConsegna());
+            preparedStatement.setDouble(7, ordine.getTotale());
+            preparedStatement.setBoolean(8, ordine.isStato());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
-            printSQLException(e);
+            e.printStackTrace();
         }
     }
 
+    @Override
     public Ordine selectOrdine(String orderID) {
         Ordine ordine = null;
         try (Connection connection = getConnection();
@@ -52,23 +55,22 @@ public class OrdineDAO implements OrdineDAOInterface{
             preparedStatement.setString(1, orderID);
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
-                String dataOrdine = rs.getDate("dataOrdine").toString();
-                String clientID = rs.getString("clientID");
+                String dataOrdine = rs.getString("dataOrdine");
+                String nomeCliente = rs.getString("nomeCliente");
+                String cognomeCliente = rs.getString("cognomeCliente");
+                String cap = rs.getString("cap");
+                String indirizzoDiConsegna = rs.getString("indirizzoDiConsegna");
                 double totale = rs.getDouble("totale");
                 boolean stato = rs.getBoolean("stato");
-                ordine = new Ordine();
-                ordine.setOrderID(orderID);
-                ordine.setDataOrdine(dataOrdine);
-                ordine.setClientID(clientID);
-                ordine.setTotale(totale);
-                ordine.setStato(stato);
+                ordine = new Ordine(orderID, dataOrdine, nomeCliente, cognomeCliente, cap, indirizzoDiConsegna, totale, stato);
             }
         } catch (SQLException e) {
-            printSQLException(e);
+            e.printStackTrace();
         }
         return ordine;
     }
 
+    @Override
     public List<Ordine> selectAllOrdini() {
         List<Ordine> ordini = new ArrayList<>();
         try (Connection connection = getConnection();
@@ -76,61 +78,47 @@ public class OrdineDAO implements OrdineDAOInterface{
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
                 String orderID = rs.getString("orderID");
-                String dataOrdine = rs.getDate("dataOrdine").toString();
-                String clientID = rs.getString("clientID");
+                String dataOrdine = rs.getString("dataOrdine");
+                String nomeCliente = rs.getString("nomeCliente");
+                String cognomeCliente = rs.getString("cognomeCliente");
+                String cap = rs.getString("cap");
+                String indirizzoDiConsegna = rs.getString("indirizzoDiConsegna");
                 double totale = rs.getDouble("totale");
                 boolean stato = rs.getBoolean("stato");
-                Ordine ordine = new Ordine();
-                ordine.setOrderID(orderID);
-                ordine.setDataOrdine(dataOrdine);
-                ordine.setClientID(clientID);
-                ordine.setTotale(totale);
-                ordine.setStato(stato);
-                ordini.add(ordine);
+                ordini.add(new Ordine(orderID, dataOrdine, nomeCliente, cognomeCliente, cap, indirizzoDiConsegna, totale, stato));
             }
         } catch (SQLException e) {
-            printSQLException(e);
+            e.printStackTrace();
         }
         return ordini;
     }
 
+    @Override
     public boolean deleteOrdine(String orderID) throws SQLException {
         boolean rowDeleted;
         try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_ORDINE_SQL)) {
-            preparedStatement.setString(1, orderID);
-            rowDeleted = preparedStatement.executeUpdate() > 0;
+             PreparedStatement statement = connection.prepareStatement(DELETE_ORDINE_SQL)) {
+            statement.setString(1, orderID);
+            rowDeleted = statement.executeUpdate() > 0;
         }
         return rowDeleted;
     }
 
+    @Override
     public boolean updateOrdine(Ordine ordine) throws SQLException {
         boolean rowUpdated;
         try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_ORDINE_SQL)) {
-            preparedStatement.setDate(1, java.sql.Date.valueOf(ordine.getDataOrdine()));
-            preparedStatement.setString(2, ordine.getClientID());
-            preparedStatement.setDouble(3, ordine.getTotale());
-            preparedStatement.setBoolean(4, ordine.isStato());
-            preparedStatement.setString(5, ordine.getOrderID());
-            rowUpdated = preparedStatement.executeUpdate() > 0;
+             PreparedStatement statement = connection.prepareStatement(UPDATE_ORDINE_SQL)) {
+            statement.setString(1, ordine.getDataOrdine());
+            statement.setString(2, ordine.getNomeCliente());
+            statement.setString(3, ordine.getCognomeCliente());
+            statement.setString(4, ordine.getCap());
+            statement.setString(5, ordine.getIndirizzoDiConsegna());
+            statement.setDouble(6, ordine.getTotale());
+            statement.setBoolean(7, ordine.isStato());
+            statement.setString(8, ordine.getOrderID());
+            rowUpdated = statement.executeUpdate() > 0;
         }
         return rowUpdated;
-    }
-
-    private void printSQLException(SQLException ex) {
-        for (Throwable e : ex) {
-            if (e instanceof SQLException) {
-                e.printStackTrace(System.err);
-                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
-                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
-                System.err.println("Message: " + e.getMessage());
-                Throwable t = ex.getCause();
-                while (t != null) {
-                    System.out.println("Cause: " + t);
-                    t = t.getCause();
-                }
-            }
-        }
     }
 }
